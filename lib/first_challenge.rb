bbac83ab5804f29b0713de42dd4183e15a0406d7
Bryna Dunnells <badunnells@gmail.com>
2019-05-07 00:19:31 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,16 @@ def first_challenge
   }
 
   #your code here
- contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_if("strawberry")
+    contacts.each do |k, data|
+      data.each do |a, v|
+        if a == :favorite_icecream_flavors
+          v.delete_if do |flavour|
+            flavour == strawberry
+          end
+        end
+      end
+    end
+          
 
   #remember to return your newly altered contacts hash!
   contacts
